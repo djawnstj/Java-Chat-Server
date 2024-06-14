e62b7db1fbb8db39460e33d35a66dffc94255ccf
JUNSEO <djawnstj44@naver.com>
2024-06-14 11:51:27 +0900

    first commit

---
+++ src/test/java/com/djawnstj/chat/JavaChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djawnstj.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
